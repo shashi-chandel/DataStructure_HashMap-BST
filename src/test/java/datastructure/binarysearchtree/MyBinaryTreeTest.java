64dd64d86cc4a110798ff3f48062d40dae0eeb61
Your Name <you@example.com>
2020-10-15 20:04:59 +0530

    [shashi] add . Create BST as shown

--- src/test/java/datastructure/binarysearchtree/MyBinaryTreeTest.java
+++ src/test/java/datastructure/binarysearchtree/MyBinaryTreeTest.java
@@ -15,4 +15,24 @@ public class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		assertEquals(3, size);
 	}
+
+	@Test
+	public void givenNumbersAsInput_WhenAddedToBinaryTree_ShouldReturnSize13() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		assertEquals(13, size);
+	}
 }
