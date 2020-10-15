b7ba19d7e187441c4cae80dcbd121655dd364254
Your Name <you@example.com>
2020-10-15 19:24:43 +0530

    [shashi] add . Find frequency in large paragraph

---
+++ src/test/java/datastructure/hashmap/MyLinkedHashMapTest.java
@@ -0,0 +1,27 @@
+package datastructure.hashmap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyLinkedHashMapTest {
+
+	@Test
+	public void givenASentence_WhenWordsAddedToList_ShouldReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting "
+				+ "themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String words[] = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		System.out.println(myLinkedHashMap);
+		int frequency = myLinkedHashMap.get("paranoid");
+		assertEquals(3, frequency);
+	}
+}
